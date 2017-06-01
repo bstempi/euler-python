d4f4a5111e9c220b5ea14106968785a7dbb02f88
Brian Stempin <brian.stempin@gmail.com>
2017-06-01 02:28:14 -0400

    Added more answers

--- euler/p1_25.py
+++ euler/p1_25.py
@@ -94,3 +94,58 @@ def problem_5():
                 break
 
     return num
+
+
+@Problem(6)
+def problem_6():
+    start, end = 1, 100
+
+    sum_of_squares = 0
+    sum_of_numbers = 0
+
+    for i in range(start, end + 1):
+        sum_of_numbers += i
+        sum_of_squares += i ** 2
+
+    square_of_sum = sum_of_numbers ** 2
+
+    return square_of_sum - sum_of_squares
+
+
+@Problem(7)
+def problem_7():
+    primes_found = 0
+    current_num = 2
+
+    while True:
+        # Is it prime?
+        is_prime = True
+        for i in range(2, int(current_num ** 0.5) + 1):
+            if i != current_num and current_num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            primes_found += 1
+        if primes_found == 10001:
+            return current_num
+
+        current_num += 1
+
+
+@Problem(8)
+def problem_8():
+    # TODO Works, but checker seems not to like it.  Opened ticket:  https://github.com/imsky/PocketEuler/issues/77
+    really_long_number = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450'
+    greatest_product = 0
+
+    for i in range(0, len(really_long_number) - 13 + 1):
+        subset = really_long_number[i:i+13]
+        product = 1
+        for char in subset:
+            product *= int(char)
+
+        if product > greatest_product:
+            greatest_product = product
+
+    return greatest_product

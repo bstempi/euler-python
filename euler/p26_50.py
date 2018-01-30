e7841b0de8a8db0cc6e1ebfa6b58ab2efa08b960
Brian Stempin <brian.stempin@gmail.com>
2018-01-30 18:45:22 -0500

    Solving 27

--- euler/p26_50.py
+++ euler/p26_50.py
@@ -60,3 +60,68 @@ def problem_26():
             j += 1
 
     return greatest_d
+
+
+@Problem(27)
+def problem_27():
+    """
+    I'm brute forcing this.  There are a few things to note, however:
+    *  We're going to be constantly testing numbers for primality, so we might as well make a set of numbers that are
+    prime below a certain number (say, a few million).  As long as the number we're considering isn't larger than the
+    last number tested when building the set, we only have to do a set lookup instead of prime factoring.  We'll find
+    this value via experimentation.
+
+    * Because we're using such a wide range of numbers, we'll calculate primes differently in this solution than we have
+    in others.  We'll use the Sieve of Eratosthenes (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
+    :return:
+    """
+
+    # Set the limit of the primes we're looking for and create a list where we set them all to prime by default
+    prime_upper_limit = 20000
+    primes = list(itertools.repeat(True, prime_upper_limit))
+    prime_set = set()
+
+    greatest_ns_a = None
+    greatest_ns_b = None
+    greatest_n = -1
+
+    # Finding primes via https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    for i in range(2, math.ceil(math.sqrt(prime_upper_limit)) + 1):
+        # If we haven't already marked the current number as non-prime, mark all of it's multiples as non-prime
+        # Also note the correction for a 0-based list
+        if primes[i-1] is True:
+            prime_set.add(i)
+
+            for k in range(2, (prime_upper_limit // i) + 1):
+                primes[i * k - 1] = False
+
+    # Ok, the outer-loop has evaluated up to sqroot(prime_upper_limit).  To find the primes larger than that, we simply
+    # iterate through the bit-list to find elements that are still marked true
+    for i in range(math.ceil(math.sqrt(prime_upper_limit)), prime_upper_limit + 1):
+        if primes[i-1] is True:
+            prime_set.add(i)
+
+    # Ok, now here's where we test all of our combinations of a and b
+    for a in range(-1000, 1000 + 1, 1):
+        for b in range(-1000, 1000 + 1, 1):
+            # Using our optimization from our function doc:  is b prime?
+            if math.fabs(b) not in prime_set:
+                continue
+
+            # n starts at 0 and goes until a break is hit
+            for n in itertools.count(0):
+                x = n**2 + (a * n) + b
+
+                # Our safeguard against our prime list being too small
+                if x > prime_upper_limit:
+                    raise Exception('The numbers being tested are larger than our pre-generated prime set')
+
+                # Our exit condition for considering this a, b combination
+                if x not in prime_set:
+                    if n - 1 > greatest_n:
+                        greatest_ns_a = a
+                        greatest_ns_b = b
+                        greatest_n = n - 1
+                    break
+
+    return greatest_ns_a * greatest_ns_b
